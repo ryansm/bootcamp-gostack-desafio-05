5a8d8b257048a1dc0a108e562739fa44f2d60410
Ryan Salles <ryansalles@nasajon.com.br>
2020-06-29 17:27:26 -0300

    Resolução

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +20,43 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const incomeArr: number[] = [];
+    const outcomeArr: number[] = [];
+
+    this.transactions.forEach(transaction => {
+      const value = transaction.value;
+
+      transaction.type === 'income'
+        ? incomeArr.push(value)
+        : outcomeArr.push(value);
+    });
+
+    const income = incomeArr.length
+      ? incomeArr.reduce((acc, val) => (acc += val))
+      : 0;
+    const outcome = outcomeArr.length
+      ? outcomeArr.reduce((acc, val) => (acc += val))
+      : 0;
+
+    const balance: Balance = {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
